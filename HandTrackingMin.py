f1ff5c557a2b03c0c21c4ec333c6bf14711fc927
Pietromuresu <muresupietro66@gmail.com>
2023-12-17 18:48:41 +0100

    divide all code as a module

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -12,7 +12,6 @@ mpDraw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
 
-
 while True:
     # Read Video frames
     success, img = cap.read()
@@ -23,7 +22,18 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            # Take info from our result
+            for id, lm in enumerate(handLms.landmark):
+
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+
+                if id == 3:
+                    cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
 
     #Calculate fps
     cTime = time.time()
